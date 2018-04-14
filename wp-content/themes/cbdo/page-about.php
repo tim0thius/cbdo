cbe90b1a5e41b7ae1812fba00eb49286016a8cf2
tim0thius <timblack.design@gmail.com>
2018-04-14 12:24:47 -0500

    Subpage setup

---
+++ wp-content/themes/cbdo/page-about.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+<section id="content" role="main">
+@wpposts
+@include( 'templates/blocks/about/about--section1' )
+@include( 'templates/blocks/about/about--section2' )
+
+@wpempty
+    <p>404</p>
+@wpend
+</section>
+<?php get_footer(); ?>
---
+++ wp-content/themes/cbdo/templates/blocks/about/about--section1.php
@@ -0,0 +1,13 @@
+<div id="about_1" class="block-highlight block-highlight--default section section--left" >
+	<div id="" class="block-highlight__inner section__inner">
+		<div class="grid grid--middle">
+			<div class="grid__item one-half">
+				<h2 class="block-highlight__headline" >{{ the_field('section_1_headline') }}</h2>
+				<div class="block-highlight__content">  {{ the_field('section_1_content') }}</div>
+			</div><!-- 
+		--><div class="grid__item one-half">
+			<?= wp_get_attachment_image(get_field('section_1_image'),'full',false,['class' => 'test-image']) ?>
+			</div>
+		</div>
+	</div>
+</div>
