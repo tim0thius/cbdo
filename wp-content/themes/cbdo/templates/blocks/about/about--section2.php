3ee06335c7adaf28be6c55c231a88e404973176d
tim0thius <timblack.design@gmail.com>
2018-04-17 09:04:09 -0500

    More ayout updates

--- wp-content/themes/cbdo/templates/blocks/about/about--section2.php
+++ wp-content/themes/cbdo/templates/blocks/about/about--section2.php
@@ -1,12 +1,12 @@
 <div id="about_2" class="block-highlight block-highlight--default section section--right" >
 	<div id="" class="block-highlight__inner section__inner">
 		<div class="grid grid--rev grid--center">
-			<div class="grid__item three-eighths portable--one-whole">
+			<div class="grid__item three-eighths palm--one-whole">
 				<h2 class="block-highlight__headline" >{{ the_field('section_2_headline') }}</h2>
 				<div class="block-highlight__content">  {{ the_field('section_2_content') }}</div>
 				<div class="block-highlight__subtext">  {{ the_field('section_2_conditions') }}</div>
 			</div><!-- 
-		--><div class="grid__item three-eighths portable--one-whole">
+		--><div class="grid__item three-eighths palm--one-whole lap--one-third">
 				<?= wp_get_attachment_image(get_field('section_2_image'),'full',false,['class' => 'block-highlight__main-image']) ?>
 			</div>
 		</div>
--- wp-content/themes/cbdo/templates/blocks/services/services--section1.php
+++ wp-content/themes/cbdo/templates/blocks/services/services--section1.php
@@ -1,13 +1,13 @@
 <div id="services_1" class="block-highlight block-highlight--default section section--left" >
 	<div id="" class="block-highlight__inner section__inner">
 		<div class="grid grid--center">
-			<div class="grid__item one-half portable--one-whole">
+			<div class="grid__item one-half palm--one-whole">
 				@if(get_field('section_1_headline'))
 				<h2 class="block-highlight__headline" >{{ the_field('section_1_headline') }}</h2>
 				@endif
 				<div class="block-highlight__content">  {{ the_field('section_1_content') }}</div>
 			</div><!-- 
-		--><div class="grid__item three-eighths portable--one-whole">
+		--><div class="grid__item three-eighths palm--one-whole lap--one-third">
 			<?= wp_get_attachment_image(get_field('section_1_image'),'full',false,['class' => 'test-image']) ?>
 			</div>
 		</div>
