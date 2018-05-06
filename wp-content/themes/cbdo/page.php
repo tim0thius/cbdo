238988f058b3aa3024bc488056770ee3d47b8730
tim0thius <timblack.design@gmail.com>
2018-05-06 15:34:56 -0500

    Carry over new subpage design

--- wp-content/themes/cbdo/page.php
+++ wp-content/themes/cbdo/page.php
@@ -2,8 +2,8 @@
 <section id="content" role="main">
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	@include( 'templates/blocks/subpage-header' )
-	<div id="post-<?php the_ID(); ?>" class="block-highlight block-highlight--default section section--left <?php post_class(); ?>" >
+	@include( 'templates/blocks/subpage-header2' )
+	<div id="post-<?php the_ID(); ?>" class="block-highlight block-highlight--default section is_main_section section--left <?php post_class(); ?>" >
 		<div id="" class="block-highlight__inner section__inner">
 			<div class="grid grid--center">
 				<div class="grid__item one-half palm--one-whole">
--- wp-content/themes/cbdo/templates/blocks/about/about--section1.php
+++ wp-content/themes/cbdo/templates/blocks/about/about--section1.php
@@ -1,12 +1,14 @@
-<div id="about_1" class="block-highlight block-highlight--default section section--left" >
+<div id="about_1" class="block-highlight block-highlight--default section section--left is_main_section" >
 	<div id="" class="block-highlight__inner section__inner">
 		<div class="grid grid--center">
 			<div class="grid__item one-half palm--one-whole">
+				@if(get_field('section_1_headline'))
 				<h2 class="block-highlight__headline" >{{ the_field('section_1_headline') }}</h2>
+				@endif
 				<div class="block-highlight__content">  {{ the_field('section_1_content') }}</div>
 			</div><!-- 
 		--><div class="grid__item three-eighths palm--one-whole lap--one-third">
-			<?= wp_get_attachment_image(get_field('section_1_image'),'full',false,['class' => 'block-highlight__main-image']) ?>
+			<?= wp_get_attachment_image(get_field('section_1_image'),'full',false,['class' => 'test-image']) ?>
 			</div>
 		</div>
 	</div>
