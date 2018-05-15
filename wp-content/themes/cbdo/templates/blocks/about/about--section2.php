98e1a304bb1d5f2335d4debd2214ba70679e2534
tim0thius <timblack.design@gmail.com>
2018-05-15 12:07:55 -0500

    Reduces vertical space above images

--- wp-content/themes/cbdo/templates/blocks/about/about--section2.php
+++ wp-content/themes/cbdo/templates/blocks/about/about--section2.php
@@ -4,7 +4,9 @@
 			<div class="grid__item three-eighths palm--one-whole">
 				<h2 class="block-highlight__headline" >{{ the_field('section_2_headline') }}</h2>
 				<div class="block-highlight__content">  {{ the_field('section_2_content') }}</div>
+				@if(get_field('section_2_conditions'))
 				<div class="block-highlight__subtext">  {{ the_field('section_2_conditions') }}</div>
+				@endif
 			</div><!-- 
 		--><div class="grid__item three-eighths palm--one-whole lap--one-third">
 				<?= wp_get_attachment_image(get_field('section_2_image'),'full',false,['class' => 'block-highlight__main-image']) ?>
