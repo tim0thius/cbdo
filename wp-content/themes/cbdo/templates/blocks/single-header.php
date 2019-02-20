3a4502c3e981f038e076ac40de617fc1cb7feda3
tim0thius <timblack.design@gmail.com>
2019-02-20 16:12:19 -0600

    Sets up article posts section

---
+++ wp-content/themes/cbdo/templates/blocks/single-header.php
@@ -0,0 +1,12 @@
+<div id="component-subpage-header" class="component-subpage-header section section--contained is-single" >
+	<div id="component-subpage-header__inner" class="component-subpage-header__inner section__inner" style="background-image: url({{ the_field('mobile_background_image', 'option') }})">
+		<div class="grid grid--full">
+			<div class="grid__item two-thirds portable--one-whole">
+				<div class="component-subpage-header__content" >
+				</div>				
+			</div><!-- 
+			 --><div class="grid__item one-third portable--one-whole" style="visibility: hidden;">
+			</div>
+		</div>
+	</div>
+</div>
